30de85af1efb6532bf9dd1788d8634f1b4695216
Yuri Claro <yuri.claro@outlook.com>
2024-04-24 14:56:51 -0300

    first commit

---
+++ backend/acusticoburguer-menu/src/main/java/com/yuriclaro/acusticoburguermenu/AcusticoburguerMenuApplication.java
@@ -0,0 +1,13 @@
+package com.yuriclaro.acusticoburguermenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcusticoburguerMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcusticoburguerMenuApplication.class, args);
+	}
+
+}
---
+++ backend/acusticoburguer-menu/src/test/java/com/yuriclaro/acusticoburguermenu/AcusticoburguerMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuriclaro.acusticoburguermenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcusticoburguerMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
