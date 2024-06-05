2e8043c1c82e213c588fb831df55df589efc8e52
Yuri Claro <yuri.claro@outlook.com>
2024-06-05 14:44:10 -0300

    feat: adicionando o SpringDoc Swagger no projeto

--- backend/acusticoburguer-menu/src/main/java/com/yuriclaro/acusticoburguermenu/controllers/FoodController.java
+++ backend/acusticoburguer-menu/src/main/java/com/yuriclaro/acusticoburguermenu/controllers/FoodController.java
@@ -4,6 +4,9 @@ package com.yuriclaro.acusticoburguermenu.controllers;
 import com.yuriclaro.acusticoburguermenu.entities.Food;
 import com.yuriclaro.acusticoburguermenu.repository.FoodRepository;
 import com.yuriclaro.acusticoburguermenu.dtos.FoodResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +19,24 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @Operation(summary = "Adicionando um novo item ao cardápio")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Item adicionado com sucesso!"),
+            @ApiResponse(responseCode = "500", description = "Houve um erro ao adicionar o item, verifique as informações")
+    })
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public void saveFood(@RequestBody FoodResponseDTO data) {
         Food foodData = new Food(data);
         repository.save(foodData);
     }
+
+    @Operation(summary = "Listando todos os itens do cardápio")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Itens listados com sucesso!"),
+            @ApiResponse(responseCode = "500", description = "Houve um erro ao listas os itens")
+    })
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
